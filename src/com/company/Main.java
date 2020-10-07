3e66b8aaa70e288641e799fd586211e177174587
knsnedden <kayleah.snedden@trojans.dsu.edu>
2020-10-07 13:33:58 -0700

    fibRecur implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    }
+
+    public static long fibRecur(long X){
+        if (X == 1 || X == 0){
+            return X;
+        }
+        else{
+            return fibRecur(X-1) + fibRecur(X-2);
+        }
     }
 }
